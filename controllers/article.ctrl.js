6e5c8d64a2229ad06fb7a432c20194971e4a72b8
“Jarman <16ucs080@lnmiit.ac.in>
2021-07-11 18:08:14 +0530

    initial setup

---
+++ controllers/article.ctrl.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+    addArticle: (req, res, next) => {},
+    getAll: (req, res, next) =>{},
+
+    /**
+     * article_id
+     */
+    clapArticle: (req, res, next) => {},
+
+    /**
+     * comment, author_id, article_id
+     */
+    commentArticle: (req, res, next) => {},
+
+    /**
+     * article_id
+     */
+    getArticle: (req, res, next) => {}
+}
